f353e4eb9e93a655f4a0798a2784b75b2eadee54
Daniel Knudsen <runedanieldk@hotmail.com>
2015-12-16 15:48:50 +0100

    Første commit

---
+++ CBD/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace CBD
+{
+    class Account
+    {
+        public double balance;
+
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public Account(double amount)
+        {
+            this.balance = amount;
+        }
+
+        public void deposit(double amount)
+        {
+            Contract.Requires(amount > 0, "Amount under 0!");
+            double oldBalance = balance;
+            balance += amount;
+            Contract.Assert(oldBalance < balance);
+        }
+
+        public void withdraw(double amount)
+        {
+            Contract.Requires(amount <= balance, "Withdraw amount larger than balance!");
+            balance -= amount;
+            Contract.Assert(balance >= 0, "Balance under 0!!");
+        }
+
+        [ContractInvariantMethod]
+        private void Invariant()
+        {
+            Contract.Invariant(balance >= 0, "Invariant: balance under 0!");
+        }
+
+    }
+}
---
+++ CBD/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace CBD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(1000.00);
+
+            account.deposit(200.00);
+
+            account.withdraw(1000.00);
+
+            account.withdraw(200.00);
+        }
+    }
+}
